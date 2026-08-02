056ba74a3f3a315a8fad73ee82682584c3333358
Param Chhabra <paramchhabra81@gmail.com>
2026-08-02 16:53:09 +0530

    Data validation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01 import DataIngestionTrainingPipeline
+from mlProject.pipeline.stage_02 import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -12,4 +13,15 @@ if __name__ == '__main__':
     except Exception as e:
         logger.exception(e)
         raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
 
